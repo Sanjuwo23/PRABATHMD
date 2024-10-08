e9bdefe257199b4fc75d20eafd0ddac8eafef6bc
sanjuw023 <sanjulasamarasinghe77@gmail.com>
2024-10-08 11:19:34 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94776928548"
+const SESSION_ID = "PRABATH-MD~4QwiGCjY#GtrwHao3SSUmSsAaWgiisgO8MvjiDmJDr1dcWKodnX4"
 
 
 module.exports = {
